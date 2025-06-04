d74f675d347560c62d3d2d0d3e97e294c7f24e1c
furkankocaoglu <159638281+furkankocaoglu@users.noreply.github.com>
2025-06-04 06:35:07 +0300

    yetkilendirme işlemleri tamamlandı.

--- bootstrapmvc/bootstrapmvc/Areas/ManagerPanel/Controllers/BlackListController.cs
+++ bootstrapmvc/bootstrapmvc/Areas/ManagerPanel/Controllers/BlackListController.cs
@@ -1,4 +1,5 @@
-﻿using bootstrapmvc.Models;
+﻿using bootstrapmvc.Areas.ManagerPanel.Filters;
+using bootstrapmvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 
 namespace bootstrapmvc.Areas.ManagerPanel.Controllers
 {
+    [ManagerLoginRequiredFilter]
     public class BlackListController : Controller
     {
         Model1 db = new Model1();
@@ -22,8 +24,16 @@ namespace bootstrapmvc.Areas.ManagerPanel.Controllers
 
             return View(cezaKontrol);
         }
-        public ActionResult ForgivePenalty(int studentId)
+        public ActionResult ForgivePenalty(int studentId)// admin ve mod rollerimiz var toplu affetme ve tekil affetme işlemleri sadece admin tarafından sağlanmaktadır.
         {
+            var manager = Session["ManagerSession"] as Manager;
+
+            if (manager == null || manager.ManagerRole_ID != 1)
+            {
+                TempData["mesaj"] = "Bu işlemi yapmaya yetkiniz yok. Admin girişi gereklidir.";
+                return RedirectToAction("Index", "BlackList");
+            }
+
             var ceza = db.Borrows.Where(b => b.StudentID == studentId && b.Penalty > 0).OrderByDescending(b => b.ReturnDate).FirstOrDefault();
 
             if (ceza == null)
@@ -39,8 +49,16 @@ namespace bootstrapmvc.Areas.ManagerPanel.Controllers
             return RedirectToAction("Index", "BlackList");
         }
 
-        public ActionResult ForgiveAllPenalties()
+        public ActionResult ForgiveAllPenalties()// admin ve mod rollerimiz var toplu affetme ve tekil affetme işlemleri sadece admin tarafından sağlanmaktadır.
         {
+            var manager = Session["ManagerSession"] as Manager;
+
+            if (manager == null || manager.ManagerRole_ID != 1)
+            {
+                TempData["mesaj"] = "Bu işlemi yapmaya yetkiniz yok. Admin girişi gereklidir.";
+                return RedirectToAction("Index", "BlackList");
+            }
+
             var cezalar = db.Borrows.Where(b => b.Penalty > 0).ToList();
             foreach (var c in cezalar)
             {
@@ -49,8 +67,9 @@ namespace bootstrapmvc.Areas.ManagerPanel.Controllers
             db.SaveChanges();
 
             TempData["mesaj"] = "Tüm öğrencilerin cezaları başarıyla affedildi.";
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "BlackList");
         }
-
     }
+
+    
 }
