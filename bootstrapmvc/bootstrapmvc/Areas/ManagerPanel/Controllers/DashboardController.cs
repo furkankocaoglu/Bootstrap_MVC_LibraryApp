a6ac6a9c11b8cc9168b096f159265476cf4b846d
furkankocaoglu <159638281+furkankocaoglu@users.noreply.github.com>
2025-06-20 17:50:17 +0300

    ..

--- bootstrapmvc/bootstrapmvc/Areas/ManagerPanel/Controllers/DashboardController.cs
+++ bootstrapmvc/bootstrapmvc/Areas/ManagerPanel/Controllers/DashboardController.cs
@@ -19,12 +19,10 @@ namespace bootstrapmvc.Areas.ManagerPanel.Controllers
             var toplam = db.Books.Count();
             var aktif = db.Books.Count(x => x.IsDeleted == false);
             var silinmis = db.Books.Count(x => x.IsDeleted == true);
-            var yazarSayisi = db.Books.Select(x => x.Writer).Distinct().Count();
 
             ViewBag.ToplamKitap = toplam;
             ViewBag.AktifKitap = aktif;
             ViewBag.SilinmisKitap = silinmis;
-            ViewBag.YazarSayisi = yazarSayisi;
 
             var model = db.Books.ToList();
             return View(model);
@@ -34,16 +32,12 @@ namespace bootstrapmvc.Areas.ManagerPanel.Controllers
             var toplamOgrenci = db.Students.Count();
             var aktifOgrenci = db.Students.Count(x => x.IsDeleted == false);
             var silinmisOgrenci = db.Students.Count(x => x.IsDeleted == true);
-            var bolumSayisi = db.Students.Select(x => x.Department).Distinct().Count();
 
             ViewBag.ToplamOgrenci = toplamOgrenci;
             ViewBag.AktifOgrenci = aktifOgrenci;
             ViewBag.SilinmisOgrenci = silinmisOgrenci;
-            ViewBag.BolumSayisi = bolumSayisi;
 
-            
             var ogrenciler = db.Students.ToList();
-
             return View(ogrenciler);
 
         }
